c2c4c8e0ca093c52039a167b41be20338dae2cd6
杜孟憲 <s981017@yahoo.com.tw>
2020-08-19 17:16:11 +0800

    api ver1

--- base_api.js
+++ base_api.js
@@ -1,227 +1,182 @@
 //-------------------------------------------API VAR-------------------------------------\\
 var api_Test_Var=0;
 var api_Youtube_Vedio_ID="";
-var api_Rnd_Var=0;
-var api_Img_Array;
-var api_Start_Array;
+/**
+ * @param {object} vdplayer to use Youtube function
+ */
+var vdplayer;
+//-----------------------------------------Inside var----------------------------------\\
+let Player;
+let a1=0,a2=0,count=1;
+let YTHeigh=0;
+let YTWidth=0;
+let YT_Start_Time_Array;     //For Youtube
+let YTswitch=0;              //For Youtube
+//----------------------------------------API Function----------------------------------\\
 
-
-  //-------------------------------------Youtube-------------------------------------\\
-  var tag = document.createElement('script');
-  tag.src = "https://www.youtube.com/iframe_api";
-  var firstScriptTag = document.getElementsByTagName('script')[0];
-  firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-  var imgArray=['https://front.nptu.edu.tw/var/file/9/1009/img/3679/004.png','https://front.nptu.edu.tw/var/file/9/1009/img/3679/003.png','https://front.nptu.edu.tw/var/file/9/1009/img/3679/002.png','https://front.nptu.edu.tw/var/file/9/1009/img/3679/001.png'];
-  let rnd= parseInt(Math.random()*10)%4;
-  let startime= parseInt(Math.random()*10)%3;
-  let startArray=[0,39,115]
-  //-------------------------------------variable-------------------------------------\\
-  function disappeared()
+/**
+ * You can use this function to Play an Animation
+ * @param ID The object ID that you want it appeares animation
+ * @param Animation_Name The name of your customized Animation
+ * @param Second The second that you want it keep
+ * @param Mode The Animaion mode . You can chooes : none | forwards | backwards | both 
+ */
+function api_Animate(ID,Animation_Name,Second,Mode)
+{
+  let condition= Animation_Name+" "+Second+" "+Mode;
+  document.getElementById(ID).style.animation =condition;
+}
+/**
+ * To go other web site
+ * @param {string} href 
+ * @param {boolean} open_new_window 
+ */
+function goto(href,open_new_window)
+{
+  if(open_new_window)
   {
-    document.getElementById("image").style.animation = "disappear 6s forwards";
-    document.getElementById("top").style.animation= "disappear2 10s forwards";
-    document.getElementById("mute").style.display="inline";
+    window.open(href);
   }
+  else if(!open_new_window)
+  {
+    document.location.href=href;
+  }
+}
+/**
+ * This function is for random play vedio or display picture
+ * @param ID The object ID that you want it random play
+ * @param api_Img_Array Input image array that it will display by random 
+ */
+function api_Media_Random(ID,api_Img_Array)
+{
+  let leng=api_Img_Array.length;
+  let rnd = parseInt(Math.random*10)%leng;
+  a1=rnd;
+  if(count)
+  {
+    document.getElementById(ID).src=api_Img_Array[rnd];
+    a2=rnd;
+    count--;
+  }
+  else if(!count)
+  {
+    if(a1 == a2)
+    {
+      if(a1 == 0)
+      {
+        a1++;
+        document.getElementById(ID).src=api_Img_Array[a2];
+        a2=a1;
+      }
+      else if(a1 == leng-1)
+      {
+        a1--;
+        document.getElementById(ID).src=api_Img_Array[a2];
+        a2=a1;
+      }
+    }
+    else if(a1 > a2 || a1 < a2)
+    {
+      document.getElementById(ID).src=api_Img_Array[a1];
+      a2=a1;
+    }
+  }
+}
+/**
+ * This function is for random play vedio or appear picture
+ * @param ID The object ID that you want it random play
+ * @param api_Vedio_Array Input vedio array to random play
+ */
+function api_Media_Random(ID,api_Vedio_Array)
+{
+  let leng=api_Vedio_Array.length;
+  let rnd = parseInt(Math.random*10)%leng;
+  a1=rnd;
+  if(count)
+  {
+    document.getElementById(ID).src=api_Vedio_Array[rnd];
+    a2=rnd;
+    count--;
+  }
+  else if(!count)
+  {
+    if(a1 == a2)
+    {
+      if(a1 == 0)
+      {
+        a1++;
+        document.getElementById(ID).src=api_Vedio_Array[a2];
+        a2=a1;
+      }
+      else if(a1 == leng-1)
+      {
+        a1--;
+        document.getElementById(ID).src=api_Vedio_Array[a2];
+        a2=a1;
+      }
+    }
+    else if(a1 > a2 || a1 < a2)
+    {
+      document.getElementById(ID).src=api_Vedio_Array[a1];
+      a2=a1;
+    }
+  }
+}
+/**
+ * Use this function to typing the parameter to ensure the window size and the Start second
+ * @param {*} YT_Player The div which you want to play youtube vedio id
+ * @param {*} Height The Height of youtube window
+ * @param {*} Width The Width of youtube window
+ * @param {string} Start_Time_Array The array of the random start time
+ */
+function YTPlayer(YT_Player,Heigh,Width,Start_Time_Array)
+{
+  Player=document.getElementById(YT_Player);
+  YTHeigh=Heigh;
+  YTWidth=Width;
+  YT_Start_Time_Array=Start_Time_Array;
+}
+/**
+ * 
+ * @param {boolean} switchCode 
+ */
+function YTSwitch(switchCode)
+{
+  YTswitch=switchCode;
+}
 
-  var vdplayer;
-  function onYouTubeIframeAPIReady() {
-  vdplayer = new YT.Player('player', {
+//-------------------------------------------Youtube API---------------------------------------\\
+function onYoutubeIframeAPIReady()
+{
+  if(YTSwitch)
+  {   
+    let Time = parseInt(Math.random*10)%api_Start_Time_Array.length;           //For Youtube
+    
+    vdplayer=new YT.Player(Player,{
     videoId: api_Youtube_Vedio_ID, // 要播放的第一支YouTube 影片ID
-    height: '1080',              // 播放器高度 (px)
-    width: '1920',               // 播放器寬度 (px)
+    height: YTHeigh,              // 播放器高度 (px)
+    width: YTWidth,
     playerVars: 
       {
-      autoplay: 1,        // 在讀取時自動播放影片
-      controls: 0,        // 在播放器顯示暫停／播放按鈕
-      showinfo: 0,        // 隱藏影片標題
-      modestbranding: 1,  // 隱藏YouTube Logo
-      loop: 1,            // 讓影片循環播放
-      playlist: api_Youtube_Vedio_ID,  //要播放的第二支YouTube 影片ID，若要重複撥放同一個影片則設為同ID即可
-      fs: 0,              // 全螢幕按鈕
-      cc_load_policty: 0, // 隱藏字幕
-      iv_load_policy: 3,  // 隱藏影片註解
-      autohide: 0,         // 當播放影片時隱藏影片控制列
-      start: startArray[startime]
-    },
-    events: {'onReady': onReady,'onStateChange': onStateChange}
-  });
-//--------
- }
-  function onReady(event)   
-  {
-    event.target.playVideo();
-    event.target.mute();
-  }
-  let mt=1;
-  function mute(e)
-  {
-    var classVal = document.getElementById("mute").getAttribute('class');
-    if(mt %2 == 0)
-    {
-      classVal = classVal.replace("fa-volume-down","fa-volume-mute");
-      document.getElementById("mute").setAttribute("class",classVal );
-      mt++;
-      vdplayer.mute();
-    }
-    else if(mt % 2 ==1)
-    {
-      classVal = classVal.replace("fa-volume-mute","fa-volume-down");
-      document.getElementById("mute").setAttribute("class",classVal );
-      mt = 0;
-      vdplayer.unMute();
-    }
-  }
-  function onStateChange(e)
-  { 
-    disappeared();
-  }
-  
-  //-------------------------ReadyState----------------------------576
-  document.onreadystatechange=function()
-  {
-    let images = document.getElementById("image");
-    let mask = document.getElementById("mask");
-    let player = document.getElementById("player");
-    var wsc=$(window).width();
-    var hsc=wsc*9/16;
-    $("#vdo").css({"height": hsc+"px"});
-    $("#vdo").css({"width":  wsc + "px"});
-    //$("#sec").text(hsc);
-    $("#base").css({"height": hsc+"px"});
-    $("#base").css({"width":  wsc + "px"});
-    $("#mask").css({"height": hsc+"px"});
-    $("#mask").css({"width":  wsc + "px"});
-    $("#image").css({"height": hsc+"px"});
-    $("#image").css({"width":  wsc + "px"});
-    
-    imageRandom();
-
-    if(wsc >= 1024)
-    {
-      player.width=wsc;
-      player.height=hsc;
-      $("#vdo").css({"height": hsc+"px"});
-      $("#vdo").css({"width":  wsc + "px"});
-      $("#mask").css({"height": hsc+"px"});
-      $("#mask").css({"width":  wsc + "px"});
-      $("#top").css({"margin-top": 0 +"px"});
-      $("#bottom").css({"margin-top": hsc-250 +"px"});
-      $("#scLogo").css({"margin-top": hsc-170 +"px"});
-      $("#scLogo").css({"margin-right": wsc*0.05 +"px"});
-      if(!TEST_Var)
-      {
-        document.getElementById("image").style.display="none";
+        autoplay: 1,        // 在讀取時自動播放影片
+        controls: 1,        // 在播放器顯示暫停／播放按鈕
+        showinfo: 0,        // 隱藏影片標題
+        modestbranding: 1,  // 隱藏YouTube Logo
+        loop: 1,            // 讓影片循環播放
+        playlist: api_Youtube_Vedio_ID,  //要播放的第二支YouTube 影片ID，若要重複撥放同一個影片則設為同ID即可
+        fs: 0,              // 全螢幕按鈕
+        cc_load_policty: 0, // 隱藏字幕
+        iv_load_policy: 3,  // 隱藏影片註解
+        autohide: 0,         // 當播放影片時隱藏影片控制列
+        start: YT_Start_Time_Array[Time]
+      },
+      events:{
+        'onReady' : onReady,
+        'onStateChange' : onStateChange
       }
-    }
+    });
+  }
+}
 
-    if( wsc < 1024 && wsc >= 576)
-    {
 
-      image.style.width=wsc;
-      image.style.height=hsc;
-      if(!TEST_Var)
-      {
-        document.getElementById("image").style.display="none";
-      }
-      document.getElementById("bottom").style.display="none";
-      $("#mask").css({"height": hsc+"px"});
-      $("#mask").css({"width":  wsc + "px"});
-      document.getElementById("player").id="play";
-      $("#base").css({"height": hsc+"px"});
-      $("#base").css({"width":  wsc + "px"});
-      $("#nptulink").css({"margin-top": hsc*1/10+"px"});
-      $("#nptulink").css({"margin-left": "50px"});
-      $("#nptulink").css({"font-size": "48pt"});
-      $("#btnn").css({"margin-top": hsc*7/10+"px"});
-      $("#btnn").css({"margin-left": "50px"});
-    }
-    else if(wsc <= 576)
-    {
-      image.style.width=wsc;
-      image.style.height=hsc;
-      if(!TEST_Var)
-      {
-        document.getElementById("image").style.display="none";
-      }
-      document.getElementById("bottom").style.display="none";
-      $("#mask").css({"height": hsc+"px"});
-      $("#mask").css({"width":  wsc + "px"});
-      $("#base").css({"height": hsc+"px"});
-      $("#base").css({"width":  wsc + "px"});
-      document.getElementById("player").id="play";
-      $("#nptulink").css({"margin-top": hsc*0.5/10+"px"});
-      $("#nptulink").css({"margin-left": "50px"});
-      $("#nptulink").css({"font-size": wsc/16+"pt"});
-      $("#btnn").css({"margin-top": hsc*6/10+"px"});
-      $("#btnn").css({"margin-left": "50px"});
-      $("#btnn").css({"font-size": wsc/36+"pt"});
-    }
-    
-  }
-  window.onresize=function()
-  {
-    let player = document.getElementById("player");
-    let image = document.getElementById("image");
-    let mask = document.getElementById("mask");
-    var wsc=$(window).width();
-    var hsc=wsc*9/16;
-    
-    //document.getElementById("size").innerText = wsc;
-    
-    if( wsc >= 1024 )
-    {
-      player.width=wsc;
-      player.height=hsc;
-      $("#base").css({"height": hsc+"px"});
-      $("#base").css({"width":  wsc + "px"});
-      $("#mask").css({"height": hsc+"px"});
-      $("#mask").css({"width":  wsc + "px"});
-      $("#top").css({"margin-top": 0 +"px"});
-      $("#bottom").css({"margin-top": hsc-195 +"px"});
-      $("#scLogo").css({"margin-top": hsc-170 +"px"});
-      $("#scLogo").css({"margin-right": wsc*0.05 +"px"});
-    }
-    
-    else if( wsc < 1024 && wsc >= 576 )
-    {
-      $("#base").css({"height": hsc+"px"});
-      $("#base").css({"width":  wsc + "px"});
-      $("#image").css({"height": hsc+"px"});
-      $("#image").css({"width":  wsc + "px"});
-      $("#mask").css({"height": hsc+"px"});
-      $("#mask").css({"width":  wsc + "px"});
-      $("#nptulink").css({"margin-top": hsc*1/10+"px"});
-      $("#nptulink").css({"margin-left": "50px"});
-      $("#nptulink").css({"font-size": "48pt"});
-      $("#btnn").css({"margin-top": hsc*7/10+"px"});
-      $("#btnn").css({"margin-left": "50px"});
-    }
 
-    if(wsc <= 560)
-    {
-      $("#base").css({"height": hsc+"px"});
-      $("#base").css({"width":  wsc + "px"});
-      $("#image").css({"height": hsc+"px"});
-      $("#image").css({"width":  wsc + "px"});  
-      $("#mask").css({"height": hsc+"px"});
-      $("#mask").css({"width":  wsc + "px"});
-      $("#nptulink").css({"margin-top": hsc*0.5/10+"px"});
-      $("#nptulink").css({"margin-left": "50px"});
-      $("#nptulink").css({"font-size": wsc/16+"pt"});
-      $("#btnn").css({"margin-top": hsc*6/10+"px"});
-      $("#btnn").css({"margin-left": "50px"});
-      $("#btnn").css({"font-size": wsc/36+"pt"});
-      
-    }
-  }
-  //------------Functions--------- 
-  
-  function imageRandom()
-  {
-    document.getElementById("image").src=imgArray[rnd];
-  }
-  function gohome()
-  {
-    document.location.href="https://www.nptu.edu.tw"
-  }
-  
